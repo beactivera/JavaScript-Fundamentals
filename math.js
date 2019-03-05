01b2610915ae9679287da0d1dcfc26d389319133
Weronika Wdowiak <weronika.wdowiak56@gmail.com>
2019-03-05 19:57:31 +0100

    math random

---
+++ math.js
@@ -0,0 +1,13 @@
+// Numbers and The Math Objects
+
+// Math Object
+val = Math.round(2.4); // 3
+val = Math.ceil(2.4);  // = 3
+val = Math.floor(2.8); // = 2
+val = Math.sqrt(64); // = 8
+val = Math.abs(-3); // absolute = 3
+val = Math.pow(8,2); // power = 64
+
+val = Math.floor(Math.random() * 20 + 1);  // random number between 1 and 20
+
+console.log(val);
